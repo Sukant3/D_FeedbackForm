3dcfc440ebfddf66f7649308917feb1cf21f29a8
Sukant3 <thombaresukant2003@gmail.com>
2025-12-02 00:01:37 +0530

    model created

---
+++ feedback_project/feedback/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Feedback(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField()
+    message=models.TextField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    
+
+
